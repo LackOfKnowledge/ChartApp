3f1e4e5aab5dd6a2b2e2613f79d665d98702b5f1
Bolko <95961186+LackOfKnowledge@users.noreply.github.com>
2023-10-07 20:34:55 +0200

    Comments deleted

--- src/components/CurrencyChart.vue
+++ src/components/CurrencyChart.vue
@@ -27,13 +27,13 @@ Chart.register(
 export default {
   props: ["targetCurrency", "baseCurrency", "startDate", "endDate"],
   mounted() {
-    // this.createChart(); // Usuń to, jeśli chcesz, aby wykres był tworzony tylko po kliknięciu przycisku
+    // this.createChart();
   },
   methods: {
     async createChart() {
       const chartData = await Promise.all(
         this.targetCurrency.map(async (targetCurrency) => {
-          console.log(`Fetching data for ${targetCurrency}`); // Dodano log konsoli
+          console.log(`Fetching data for ${targetCurrency}`);
           const data = await CurrencyService.fetchCurrencyData(
             this.baseCurrency,
             targetCurrency,
@@ -41,10 +41,9 @@ export default {
             this.endDate
           ).catch((error) =>
             console.error(`Error fetching data for ${targetCurrency}:`, error)
-          ); // Dodano obsługę błędów
-          console.log(`Data for ${targetCurrency}:`, data); // Dodano log konsoli
+          );
+          console.log(`Data for ${targetCurrency}:`, data);
           if (data && Array.isArray(data.rates)) {
-            // Dodano sprawdzanie błędów
             return {
               label: targetCurrency,
               data: data.rates.map((rate) => rate[targetCurrency]),
@@ -57,7 +56,6 @@ export default {
       );
 
       if (chartData[0] && chartData[0].data) {
-        // Dodano sprawdzanie błędów
         new Chart(
           document.getElementById("currencyChart"),
           console.log(this.startDate, this.endDate),
